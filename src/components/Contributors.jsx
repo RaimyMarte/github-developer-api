781d7485d980835b350cd0c22ce8e46fbceaa128
Raimy Marte <raimy241@gmail.com>
2023-05-01 19:05:43 -0400

    view more button

--- src/components/Contributors.jsx
+++ src/components/Contributors.jsx
@@ -2,12 +2,20 @@ import { useParams } from "react-router-dom"
 import { useFetch } from "../hooks"
 import { Loading } from "./Loading"
 import { ContributorsCard } from "./ContributorsCard"
+import { useState } from "react"
 
 export const Contributors = () => {
 
     const { id } = useParams()
-    const { data, isLoading } = useFetch(`https://api.github.com/repositories/${id}/contributors?per_page=10`)
-console.log(data)
+    const { data, isLoading } = useFetch(`https://api.github.com/repositories/${id}/contributors`)
+
+    const [userCount, setUserCount] = useState(10);
+
+    const handleViewMore = () => {
+        setUserCount(prevCount => prevCount + 5);
+    };
+
+
     return (
         <>
             <h1>Top Contributors</h1>
@@ -15,14 +23,20 @@ console.log(data)
                 isLoading
                     ? <Loading />
 
-                    : data.map(user => (
+                    :
+                    data.slice(0, userCount).map(user => (
                         <ContributorsCard
                             key={user.id}
-
                             {...user}
                         />
                     ))
             }
+            <button
+                className="btn btn-primary"
+                onClick={handleViewMore}
+            >
+                View More
+            </button>
         </>
     )
 }
